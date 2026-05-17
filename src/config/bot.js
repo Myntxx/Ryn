66cff7d00846f82e3355d9db6061607db72fc7c6
Myntxx <matheomassart@icloud.com>
2026-05-17 23:21:23 +0200

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Developed by Mynt",
+        name: "Par Mynt",
         // Activity type number (0 = Playing).
         type: 0, 
       },
